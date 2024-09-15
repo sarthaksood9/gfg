83840b88799c3478da4b4838694f5fd348faa96b
sarthaksood9 <sarthaksood9@gmail.com>
2024-09-16 05:17:37 +0530

    hi

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+
+import Login from './components/Login'
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom'
+import Home from './components/Homepage'
+import About from './components/About'
+import ProductRoutees from './Routes/ProductRoutees';
+import AdminRoutes from './Routes/AdminRoutes';
+import { Toaster } from 'react-hot-toast';
+function App() {
+
+
+  const user = { user: { role: "admin" } };
+
+  console.log(user?.user?.role);
+  // console.log(user?.user?.role);
+
+  return (
+    <>
+      {/* <BrowserRouter> */}
+      <Router>
+        <Routes>
+
+          {<Route path='/*' element={user?.user?.role !== "admin" ? <AdminRoutes /> : <ProductRoutees />} />}
+        </Routes>
+      </Router>
+          <Toaster />
+      {/* </BrowserRouter> */}
+
+    </>
+  )
+}
+
+export default App
